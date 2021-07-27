09cd783a2da018ece911e01dc918a9896c130da7
oskar <ditdo@DESKTOP-KKPBGO4>
2021-07-27 09:06:46 +0200

    zalozeni gitu základy

---
+++ src/main/java/net/mcreator/perky/item/SpagetiItem.java
@@ -0,0 +1,51 @@
+
+package net.mcreator.perky.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.UseAction;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.Food;
+import net.minecraft.client.util.ITooltipFlag;
+
+import net.mcreator.perky.PerkyModElements;
+
+import java.util.List;
+
+@PerkyModElements.ModElement.Tag
+public class SpagetiItem extends PerkyModElements.ModElement {
+	@ObjectHolder("perky:spageti")
+	public static final Item block = null;
+	public SpagetiItem(PerkyModElements instance) {
+		super(instance, 5);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new FoodItemCustom());
+	}
+	public static class FoodItemCustom extends Item {
+		public FoodItemCustom() {
+			super(new Item.Properties().group(ItemGroup.FOOD).maxStackSize(64).rarity(Rarity.COMMON)
+					.food((new Food.Builder()).hunger(4).saturation(0.3f).build()));
+			setRegistryName("spageti");
+		}
+
+		@Override
+		public UseAction getUseAction(ItemStack itemstack) {
+			return UseAction.EAT;
+		}
+
+		@Override
+		public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("italiano"));
+		}
+	}
+}
